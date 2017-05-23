329a23a52dbc83346e06dc8bd0abf54a42305d07
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2017-05-23 21:58:19 +0800

    Migrate to new proptypes package to prevent console warnings from popping up (#4)

--- src/components/Col.jsx
+++ src/components/Col.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { getClass, isSizeValid, pushSizeClassNames } from '../shared/utils';
 
 const Col = ({
